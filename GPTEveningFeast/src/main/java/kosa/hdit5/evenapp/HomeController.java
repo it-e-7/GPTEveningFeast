1c2e42411da3014edf6fe4beb0abe0efe8619551
qmdl980 <qmdl98056@gmail.com>
2023-05-22 11:57:15 +0900

    fix : 설정 수정

--- GPTEveningFeast/src/main/java/kosa/hdit5/evenapp/HomeController.java
+++ GPTEveningFeast/src/main/java/kosa/hdit5/evenapp/HomeController.java
@@ -1,11 +1,6 @@
 package kosa.hdit5.evenapp;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,16 +14,7 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		String abc = "abc";
+	public String home() {
 		
 		return "home";
 	}
