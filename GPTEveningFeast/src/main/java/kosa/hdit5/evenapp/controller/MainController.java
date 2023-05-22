e5be5c5f49caad7ba78e8fbb71d82c5cd6732856
qmdl980 <qmdl98056@gmail.com>
2023-05-22 17:23:14 +0900

    refactor : MVC 구조로 수정 (sample code)

---
+++ GPTEveningFeast/src/main/java/kosa/hdit5/evenapp/controller/MainController.java
@@ -0,0 +1,28 @@
+package kosa.hdit5.evenapp.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kosa.hdit5.evenapp.service.MainService;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	Logger log = LogManager.getLogger("case3");
+	
+	@Autowired
+	MainService service; 
+	
+	@GetMapping
+	public String mainHandler() {
+		
+		log.debug(service.getAllUser());
+		
+		return "home";
+	}
+}
